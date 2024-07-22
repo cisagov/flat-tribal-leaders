06842211bafc3ebf67bbbba98b8fb108fbf65866
matthewswspence <Matthewswspence@gmail.com>
2024-07-22 17:45:28 -0500

    fix postprocess script locally

--- postprocess.py
+++ postprocess.py
@@ -4,11 +4,12 @@ import pandas as pd
 if __name__ == "__main__":
 
     # args will be forwarded to sys.argv exactly as they were in Deno, aka a list of strings
+    print(sys.argv[1])
 
     # the very first arg will be the downloaded filename 
-    df = pd.from_csv(sys.argv[0])
-    df = df.drop('OBJECTID', axis=1)
-    df = df.sort_values('0')
+    df = pd.read_csv(sys.argv[1])
+    df.drop('OBJECTID', axis=1, inplace=True)
+    df = df.sort_values('Tribe Full Name')
 
     # write edited file to repo. Pandas will overwrite by default.
-    df.to_csv("tribal-leaders.csv")
+    df.to_csv("tribal-leaders.csv",index=False)
