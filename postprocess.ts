ba5fa8bd59e308c26953317bc9e2e88065f97f85
matthewswspence <Matthewswspence@gmail.com>
2024-07-24 09:45:26 -0500

    strip newline in postprocess

--- postprocess.ts
+++ postprocess.ts
@@ -1,8 +1,9 @@
 import { readCSV, writeCSV, removeFile } from 'https://deno.land/x/flat@0.0.15/mod.ts' // replace with latest library https://deno.land/x/flat@0.0.x/mod.ts
 
+// flat action runs on Deno, a Node competitor. Flat will pass in the downloaded filename as arg 0.
 const filePath: string = Deno.args[0]
 
-// flat runs on Deno, a Node competitor. Flat will pass in the downloaded filename as arg 0.
+// without lazyQuotes some of the values break readCSV
 const tribalLeaders: Record<string, any> = await readCSV(filePath, {lazyQuotes: true})
 
 // custom compare function
@@ -16,12 +17,22 @@ function compare( a, b ) {
     return 0;
   }
 
-// remove OBJECTID
+// process each line
 tribalLeaders.forEach(object => {
+    // remove OBJECTID
     delete object['OBJECTID'];
+
+    //strip newlines from all but the last element
+    for(const property in object) {
+        if(property === 'y') {
+            break;
+        }
+        object[property] = object[property].replace(/\r?\n|\r/g, '');
+    }
  })
 
+ // sort using custom compare function
 tribalLeaders.sort(compare)
 
-await writeCSV('./tribal-leaders.csv', tribalLeaders)
+await writeCSV('./tribal-leaders-test.csv', tribalLeaders)
 
