58997660d9dab316f4f10e7896b7ad6863af72c0
matthewswspence <Matthewswspence@gmail.com>
2024-07-29 10:45:58 -0500

    Review edits

--- postprocess.ts
+++ postprocess.ts
@@ -1,9 +1,10 @@
-import { readCSV, writeCSV, removeFile } from 'https://deno.land/x/flat@0.0.15/mod.ts' // replace with latest library https://deno.land/x/flat@0.0.x/mod.ts
+import { readCSV, writeCSV, removeFile } from 'https://deno.land/x/flat@0.0.15/mod.ts' 
+// replace with latest library as needed: https://deno.land/x/flat@0.0.x/mod.ts
 
 // flat action runs on Deno, a Node competitor. Flat will pass in the downloaded filename as arg 0.
 const filePath: string = Deno.args[0]
 
-// without lazyQuotes some of the values break readCSV
+// Lazyquotes lets readCSV handle values containing line-endings inside quoted strings. 
 const tribalLeaders: Record<string, any> = await readCSV(filePath, {lazyQuotes: true})
 
 // custom compare function
@@ -11,7 +12,7 @@ function compare( a, b ) {
     if ( a["Tribe Full Name"] < b["Tribe Full Name"] ){
       return -1;
     }
-    if ( a['Tribe Full Name'] > b['Tribe Last Name'] ){
+    if ( a['Tribe Full Name'] > b['Tribe Full Name'] ){
       return 1;
     }
     return 0;
